0185b1b74c3fe4ad4a7c41171ae78e51a514b62f
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-12 12:53:59 +0900

    Understanding Modules

---
+++ src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get('/:id/:optional?')
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(request);
+    console.log(headers);
+    console.log(ip);
+    return 'You sent a post request to users endpoint';
+  }
+}

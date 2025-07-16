b1a20c86ef6061bf33d223145e63a8601a2ef2c5
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-16 19:23:44 +0900

    Postgresと接続＆ユーザーのエンティティとレポジトリ追加

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,13 +1,33 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+
+  public async createuser(createUserDto: CreateUserDto) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    // Handle exception
+
+    const newUser = this.usersRepository.create(createUserDto);
+    await this.usersRepository.save(newUser);
+
+    return newUser;
+  }
+
   public findAll(
     getuserParamDto: GetUsersParamDto,
     limit: number,

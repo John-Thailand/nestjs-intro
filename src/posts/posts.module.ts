5b72c73e1fc5416c4a7d68e6e581da91d72965b4
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-17 22:52:25 +0900

    PostとMetaOptionをOneToOneで紐づける

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,9 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './providers/posts.service';
 import { UsersModule } from 'src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
 
 @Module({
   controllers: [PostsController],

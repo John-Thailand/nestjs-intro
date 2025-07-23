af0a2b162489d434826f3df70f3fe2353dda77a9
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-23 13:42:23 +0900

    NODE_ENVのデフォルトをdevelopmentに設定

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 export default Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'test', 'production', 'staging')
-    .default(),
+    .default('development'),
   DATABASE_PORT: Joi.number().port().default(5432),
   DATABASE_PASSWORD: Joi.string().required(),
   DATABASE_HOST: Joi.string().required(),

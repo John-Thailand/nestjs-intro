1f645b3d48a331fcb56682780079d13ece2bef9e
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-29 18:21:46 +0900

    UploadsServiceを実装

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { UploadToAwsProvider } from './upload-to-aws.provider';
+import { Repository } from 'typeorm';
+import { Upload } from '../upload.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { UploadFile } from '../interfaces/upload-file.interface';
+import { fileTypes } from '../enums/file-types.enum';
 
 @Injectable()
 export class UploadsService {
+  constructor(
+    @InjectRepository(Upload)
+    private readonly uploadsRepository: Repository<Upload>,
+    private readonly configService: ConfigService,
+    private readonly uploadToAwsProvider: UploadToAwsProvider,
+  ) {}
   public async uploadFile(file: Express.Multer.File) {
-    // Upload to the file to AWS S3
-    // Generate to a new entry in database
+    // throw error for unsupported MIME types
+    if (
+      !['image/gif', 'image/jpeg', 'image/jpg', 'image/png'].includes(
+        file.mimetype,
+      )
+    ) {
+      throw new BadRequestException('Mime type not supported');
+    }
+    try {
+      // Upload to the file to AWS S3
+      const name = await this.uploadToAwsProvider.fileUpload(file);
+      // Generate to a new entry in database
+      const uploadFile: UploadFile = {
+        name: name,
+        path: `https://${this.configService.get('appConfig.awsCloudfrontUrl')}/${name}`,
+        type: fileTypes.IMAGE,
+        mime: file.mimetype,
+        size: file.size,
+      };
+      const upload = this.uploadsRepository.create(uploadFile);
+      return await this.uploadsRepository.save(upload);
+    } catch (error) {
+      throw new ConflictException(error);
+    }
   }
 }
